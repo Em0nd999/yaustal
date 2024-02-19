5255626979c263f9ca8754618e18eb915c0b659a
Em0nd999 <Em9nd999@gmail.com>
2024-02-19 21:30:59 +0600

    second commit

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField("Caption", max_length=50)
+    description = models.TextField("Content")
+    image = models.CharField("URL image", max_length=500)
+    post_date = models.DateTimeField("Date and publication time", default=datetime.now)
+    
+    def __str__(self):
+        return f"{self.title}"
--- news_app/views.py
+++ news_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def home_page(request):
+    posts = Post.objects.all()[:4]
+    return render(request, "./primary/index.html", {'posts': posts})
+
+def news_page(request):
+    return render(request, "./primary/news.html")
+
+def news_detail_page(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "./primary/news-detail.html", {'post': post})
